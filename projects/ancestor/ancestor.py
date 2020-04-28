a80071c1f5c611b5c646be2885989b26909afb8e
Ryan Ansberry <ansman92@gmail.com>
2020-04-27 18:28:28 -0700

    ancestors.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,53 @@
+from graph import Graph
+from queue import Queue
+
+# breath first treversal
+# returns array of all paths related to given node
+def bft_find_all_paths(graph, starting_node):
+    # set up queue, visited, and a list of paths
+	queue = Queue()
+	queue.enqueue([starting_node])
+	visited = set()
+	all_paths = []
+
+	while queue.size() > 0:
+		path = queue.dequeue()
+		node = path[-1]
+
+		if node not in visited:
+			visited.add(node)
+			if len(graph.nodes[node]) > 0:
+				all_paths = []
+			for new_node in graph.nodes[node]:
+				new_path = list(path)
+				new_path.append(new_node)
+				if len(graph.nodes[new_node]) == 0:
+					all_paths.append(new_path)
+				else:
+					queue.enqueue(new_path)
+
+	return all_paths
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+	graph = Graph()
+
+	# populates graph
+	for ancestor in ancestors:
+		graph.add_edge(ancestor[0], ancestor[1])
+
+
+	all_paths = bft_find_all_paths(graph, starting_node)
+
+	# assigns -1 if no paths exist otherwise assigns last value of first path 
+	answer = -1 if len(all_paths) == 0 else all_paths[0][-1]
+
+	if len(all_paths) > 1:
+		for i in range(len(all_paths)-1):
+			if all_paths[i+1][-1] < answer:
+				answer = all_paths[i+1][-1]
+
+	return answer
+
+
+# (parent, child)
+
---
+++ projects/ancestor/graph.py
@@ -0,0 +1,13 @@
+class Graph:
+    def __init__(self):
+        self.nodes = {}
+
+    def add_node(self, node):
+        self.nodes[node] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 not in self.nodes:
+            self.add_node(v1)
+        if v2 not in self.nodes:
+            self.add_node(v2)
+        self.nodes[v2].add(v1)
